bf367a262c009bb5bb69316e81a125b785f20fe1
OmarKHDR <omarsliman37@gmail.com>
2024-06-17 18:40:30 +0300

    adding 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - entry
+ * Description : this is simple
+ * Return: 0 - just a zero n
+*/
+
+int main(void)
+{
+	pritnf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
