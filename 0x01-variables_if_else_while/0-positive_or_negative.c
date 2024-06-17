b69269d1a7043f01da4613c57949d7a502822c47
OmarKHDR <omarsliman37@gmail.com>
2024-06-18 00:58:05 +0300

    ffer

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,6 +7,7 @@
 * main - this is main
 * Description: this is des
 * Return: 0
+*
 */
 int main(void)
 {
@@ -17,7 +18,7 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive", n);
 	else if (n == 0)
-		printf("%d is zero");
+		printf("%d is zero", n);
 	else
 		printf("%d is negative", n);
 	return (0);
