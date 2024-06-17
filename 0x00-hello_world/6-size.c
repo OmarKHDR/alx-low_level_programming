331bb05af2e942eca2c8680273bf22769ad0dab3
OmarKHDR <omarsliman37@gmail.com>
2024-06-17 18:51:09 +0300

    adding 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - entry
+ * Description : this is simple
+ * Return: 0 - just a zero n
+*/
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+	return (0);
+}
