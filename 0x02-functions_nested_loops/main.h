111fccd5ee460991066621bbacbe65f1c3351b63
OmarKHDR <omarsliman37@gmail.com>
2024-06-18 01:42:06 +0300

    adding 0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 int _putchar(char c);
@@ -16,6 +15,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n)
-	
 
 #endif
