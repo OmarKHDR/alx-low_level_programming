d7bb1bc961ab589477f217e22b7fa4578e946aa5
OmarKHDR <omarsliman37@gmail.com>
2024-06-18 18:55:07 +0300

    adding 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - my func
+ * @c: the arg
+ * Description: This function uses _putchar to print to the
+ * standard output
+ * Return: int
+*/
+int _islower(int c)
+{
+	return (islower(c));
+}
