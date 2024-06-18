ba06390e9b1c0a674f791366f39cfdcdf8f631cf
OmarKHDR <omarsliman37@gmail.com>
2024-06-18 20:31:25 +0300

    adding 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
-*
-*
+* main - main just it
+* Description: it is what it is
+* Return: a number
 */
 
-int main()
+int main(void)
 {
-	int i=0, sum=0;
+	int i = 0, sum = 0;
 
-	while(i < 1024)
+	while (i < 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-			sum+=i;
+			sum += i;
 		i++;
 	}
 	printf("%d\n", sum);
