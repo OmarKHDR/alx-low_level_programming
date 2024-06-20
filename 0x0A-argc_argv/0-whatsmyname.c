0cbd6a8193da4097b8f7a290059ada343e91d599
OmarKHDR <omarsliman37@gmail.com>
2024-06-20 19:01:39 +0300

    ./gitit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
 * Description: Description
 * @argv: arg array
 * @argc: count
-* Return: 0 
+* Return: 0
 */
 
-int main (int argc,char **argv)
+int main(int argc, char **argv)
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
