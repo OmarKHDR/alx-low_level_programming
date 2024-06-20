0c7665c131aa73e856118ce60ff83e8eb81f805d
OmarKHDR <omarsliman37@gmail.com>
2024-06-20 19:17:07 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main
+* Description: Description
+* @argv: arg array
+* @argc: count
+* Return: 0
+*/
+
+int main(int args, char **argv)
+{
+	printf("%d\n", args);
+	(void)args;
+	return (EXIT_SUCCESS);
+}
