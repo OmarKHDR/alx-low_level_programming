c208a8c3e2eb081424333bbd0b7daf4986dddda0
OmarKHDR <omarsliman37@gmail.com>
2024-06-20 22:44:31 +0300

    adding 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,12 +11,11 @@
 
 int **alloc_grid(int width, int height)
 {
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
 	int i;
 	int **arrptr = (int **)calloc(height, sizeof(int *));
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		arrptr[i] = (int *)calloc(width, sizeof(int));
