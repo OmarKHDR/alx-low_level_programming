de67459e957037597a58042601861a102066f667
OmarKHDR <omarsliman37@gmail.com>
2024-06-20 22:01:34 +0300

    adding 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,16 +4,18 @@
 /**
 * create_array - c
 * Description: create_array
-* 
+* @size: arr size
+* @c: arr filler
 * Return: [pointer to arr]
 */
 char *create_array(unsigned int size, char c)
 {
 	int i = 0;
 	char *arr = (char *) malloc(sizeof(char) * size);
+
 	if (arr == NULL)
-		return NULL;
-	for (; i<size; i++)
+		return (NULL);
+	for (; i < size; i++)
 		arr[i] = c;
-	return arr;
+	return (arr);
 }
