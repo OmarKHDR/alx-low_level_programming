aeef8bc70beb695f839adf8a37f43b20a7221883
OmarKHDR <omarsliman37@gmail.com>
2024-06-21 14:57:24 +0300

    adding 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int i;
 
 	if (separator == NULL)
-		return;
+		separator = "";
 	va_start(ptr, n);
 	for (i = 0; (unsigned int)i < n; i++)
 	{
