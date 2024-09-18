3973e7ce8df0eab23262d9898f2e9af13dbc3772
OmarKHDR <omarsliman37@gmail.com>
2024-09-18 14:38:53 +0300

    pls

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,36 +11,15 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (!(*s1) && !(*s2))
-	{
-		return (1);
-	}
-
-	if (*s2 == '*')
-	{
-		while (*s2 == '*')
-		{
-			s2++;
-		}
-
-		if (!(*s2))
-		{
-			return (1);
-		}
-		while (*s1)
-		{
-			if (wildcmp(s1, s2))
-			{
+		if (!(*s1) && !(*s2)){
 				return (1);
-			}
-			s1++;
+		}
+
+		if (*s2 == '*'){
+				return wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2));
+		}
+		if (*s1 == *s2){
+				return wildcmp(s1 + 1, s2 + 1);
 		}
 		return (0);
-	}
-
-	if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	return (0);
 }
